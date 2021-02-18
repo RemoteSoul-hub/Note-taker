3f12c4f33b672e2267541dea19fa70a6c9bb0c76
RemoteSoul-Hub <yahya.tahoune@gmail.com>
2021-02-18 23:47:13 +0100

    added baseconcept. Adding the note in a table

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+let submit = document.getElementById('submit');
+let note = document.getElementById('field');
+let table = document.getElementById('notes');
 // Event listener to Submit click button
 
+submit.addEventListener('click', addNote);
 
 // Function to add text to table block
-
+function addNote(e) {
+ e.preventDefault();
+    let note = document.getElementById('field').value;
+    let row = table.insertRow(1);
+    let cell = row.insertCell(0);
+    cell.innerHTML = note;
+}
 
 // two elements per row only
 
